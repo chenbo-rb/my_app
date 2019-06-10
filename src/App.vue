ce7716d13cb6189db557f83cb4fda8e511fcbed5
chenbo-rb <chenbo_rb@163.com>
2019-06-10 11:24:15 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{msg}}
+    <div>
+      <input type="text" v-model="info">
+      <button v-on:click="handleClick">添加</button>
+    </div>
+    <ul>
+      <todo-item v-for="item in list" :key="item">
+        <template v-slot:item="itemProps">
+          <span :style="{fontSize:'20px',color:itemProps.checked?'red':'blue'}">{{item}}</span>
+        </template>
+      </todo-item>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todoItem from "./components/ToDoItem.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      msg: "hello vue ......",
+      info: "",
+      list: []
+    };
+  },
+  methods: {
+    handleClick() {
+      this.list.push(this.info);
+      this.info = "";
+    }
+  },
   components: {
-    HelloWorld
+    todoItem
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
